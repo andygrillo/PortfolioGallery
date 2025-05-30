bba270decb2a172e161092bf48038c3bf9861adc
Andres Meira <andresmeira@gmail.com>
2025-05-30 13:14:06 -0600

    2

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   apiEndpoint: process.env.NODE_ENV === 'production'
-    ? 'https://YOUR_API_GATEWAY_ID.execute-api.YOUR_REGION.amazonaws.com/prod'
+    ? 'https://uc00uuzw02.execute-api.us-east-1.amazonaws.com/prod'
     : 'http://localhost:3001'
 };
