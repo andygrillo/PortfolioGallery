f42358589afcdb1bea028dc59646f9bbc88ac506
Andres Meira <andresmeira@gmail.com>
2025-05-30 13:51:42 -0600

    1

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,3 @@
 export const config = {
-  apiEndpoint: process.env.NODE_ENV === 'production'
-    ? process.env.VITE_API_ENDPOINT
-    : 'http://localhost:3001'
+  apiEndpoint: import.meta.env.VITE_API_ENDPOINT || 'http://localhost:3001'
 };
